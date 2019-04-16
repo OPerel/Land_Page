471a32a4665a3fd7fb92c30fcc78c26f753c4a4d
Operel <oriperelman@gmail.com>
2019-04-16 12:01:01 +0300

    add section. needs responsive view adjusments

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function() {
    $('#scroll').click (function() {
-     $('html, body').animate({scrollTop: $('.services').offset().top }, 2000);
+     $('html, body').animate({scrollTop: $('.services').offset().top }, 1400);
      return false;
    });
  });
