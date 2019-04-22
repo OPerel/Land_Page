aa8d4b45d6b0d3a0b1ebc2717e8288196a4e70cb
Operel <oriperelman@gmail.com>
2019-04-22 23:07:43 +0300

    enable jquery scroll func

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 // Jquery - replace with react
-// $(function() {
-//    $('#scroll').click (function() {
-//      $('html, body').animate({scrollTop: $('.services').offset().top }, 1400);
-//      return false;
-//    });
-//  });
+$(function() {
+   $('#scroll').click (function() {
+     $('html, body').animate({scrollTop: $('.services').offset().top }, 1400);
+     return false;
+   });
+ });
 
 // React
 class Nav extends React.Component {
