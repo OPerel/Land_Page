6d7a6b4dc48cf140286b73344ce9f57515e5de0b
Operel <oriperelman@gmail.com>
2019-04-22 23:00:28 +0300

    create nav as a react component

--- src/script.js
+++ src/script.js
@@ -1,21 +1,38 @@
 // Jquery - replace with react
-$(function() {
-   $('#scroll').click (function() {
-     $('html, body').animate({scrollTop: $('.services').offset().top }, 1400);
-     return false;
-   });
- });
-
+// $(function() {
+//    $('#scroll').click (function() {
+//      $('html, body').animate({scrollTop: $('.services').offset().top }, 1400);
+//      return false;
+//    });
+//  });
 
 // React
 class Nav extends React.Component {
-    super();
-    constructor() {
+    constructor () {
+        super ();
         this.state = {
             page: 'header'
-        }
-    }
-}
+        };
+    };
 
-const nav = document.getElementsByTagName('nav');
+    render () {
+        const page = this.state.page;
+        return (
+            <ul>
+                <li><a href=""><i id="logo" className="fab fa-hooli"></i></a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Fetures</a></li>
+                <li><a href="">Products</a></li>
+            </ul>
+        );
+    };
+};
+
+// const Nav = () => {
+//     return (
+//
+//     );
+// };
+
+const nav = document.getElementById('nav');
 ReactDOM.render(<Nav />, nav);
