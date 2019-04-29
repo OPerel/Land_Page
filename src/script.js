629b7edeb6c132575403fd9948eeb4eeaae39659
Operel <oriperelman@gmail.com>
2019-04-29 16:16:57 +0300

    remove scrolling

--- src/script.js
+++ src/script.js
@@ -1,68 +1,68 @@
 const nav = document.getElementById('nav');
-const dropNav = document.getElementById('drop-container');
+// const dropNav = document.getElementById('drop-container');
 const mButton = document.getElementById('scroll')
 
-const DeskTopNav = ({ onClickLink }) => {
+const DeskTopNav = () => {
     return (
         <ul>
-            <li><a onClick={(e) => onClickLink('h')}>
+            <li><a>
                 <i id="logo" className="fab fa-hooli"></i>
             </a></li>
-            <li><a onClick={(e) => onClickLink('f')}>Fetures</a></li>
-            <li><a onClick={(e) => onClickLink('p')}>Products</a></li>
-            <li><a onClick={(e) => onClickLink('s')}>Contact</a></li>
+            <li><a>Fetures</a></li>
+            <li><a>Products</a></li>
+            <li><a>Contact</a></li>
         </ul>
     );
 };
 
-const MobileNav = ({ onClickLink }) => {
-    const barIcon = document.getElementById('bar');
-
-    const toggleDropDown = () => {
-        document.getElementById('dropdown').classList.toggle('drop');
-    }
-
-    return (
-        <div>
-            <ul>
-                <li>
-                    <a onClick={(e) => onClickLink('h')}>
-                        <i id="logo" className="fab fa-hooli"></i>
-                    </a>
-                </li>
-                <li>
-                    <a id="bar" onClick={toggleDropDown}>
-                        <i className="fas fa-bars"></i>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    );
-
-    barIcon.addEventListener('click', toggleDropDown)
-};
-
-const DropDown = ({ onClickLink }) => {
-    return (
-        <ul id="dropdown" className="drop">
-            <li><a onClick={(e) => onClickLink('f')}>Fetures</a></li>
-            <li><a onClick={(e) => onClickLink('p')}>Products</a></li>
-            <li><a onClick={(e) => onClickLink('s')}>Contact</a></li>
-        </ul>
-    );
-};
+// const MobileNav = ({ onClickLink }) => {
+//     const barIcon = document.getElementById('bar');
+//
+//     const toggleDropDown = () => {
+//         document.getElementById('dropdown').classList.toggle('drop');
+//     }
+//
+//     return (
+//         <div>
+//             <ul>
+//                 <li>
+//                     <a onClick={(e) => onClickLink('h')}>
+//                         <i id="logo" className="fab fa-hooli"></i>
+//                     </a>
+//                 </li>
+//                 <li>
+//                     <a id="bar" onClick={toggleDropDown}>
+//                         <i className="fas fa-bars"></i>
+//                     </a>
+//                 </li>
+//             </ul>
+//         </div>
+//     );
+//
+//     barIcon.addEventListener('click', toggleDropDown)
+// };
+//
+// const DropDown = ({ onClickLink }) => {
+//     return (
+//         <ul id="dropdown" className="drop">
+//             <li><a onClick={(e) => onClickLink('f')}>Fetures</a></li>
+//             <li><a onClick={(e) => onClickLink('p')}>Products</a></li>
+//             <li><a onClick={(e) => onClickLink('s')}>Contact</a></li>
+//         </ul>
+//     );
+// };
 
 class Nav extends React.Component {
     constructor () {
         super ();
         this.state = {
-            screenSize: window.innerWidth
+            // screenSize: window.innerWidth
         };
     };
 
-    onChangeViewPort = (event) => {
-        this.setState({ screenSize: event.target.innerWidth });
-    }
+    // onChangeViewPort = (event) => {
+    //     this.setState({ screenSize: event.target.innerWidth });
+    // }
 
     changeNavBg = () => {
         if (window.scrollY > 100) {
@@ -72,37 +72,39 @@ class Nav extends React.Component {
         }
     }
 
-    scrollToSection = (sec) => {
-        const dropDown = document.getElementById('dropdown')
-        let section = document.getElementById(sec);
-        window.scrollTo({
-            top: section.offsetTop - 80,
-            behavior: 'smooth'
-        });
-        if (sec !== 'h') {
-            dropDown.classList.toggle('drop');
-        }
-    };
+    // scrollToSection = (sec) => {
+    //     const dropDown = document.getElementById('dropdown')
+    //     let section = document.getElementById(sec);
+    //     window.scrollTo({
+    //         top: section.offsetTop - 80,
+    //         behavior: 'smooth'
+    //     });
+    //     if (sec !== 'h') {
+    //         dropDown.classList.toggle('drop');
+    //     }
+    // };
 
     componentDidMount () {
-        window.addEventListener('resize', this.onChangeViewPort)
+        // window.addEventListener('resize', this.onChangeViewPort)
         window.addEventListener('scroll', this.changeNavBg);
-        mButton.addEventListener('click', (e) => this.scrollToSection('f'));
+        // mButton.addEventListener('click', (e) => this.scrollToSection('f'));
     }
 
     render () {
-        if (this.state.screenSize > 992) {
-            return (
-                <DeskTopNav onClickLink={this.scrollToSection} />
-            );
-        } else {
-            return (
-                <div>
-                    <MobileNav onClickLink={this.scrollToSection} />
-                    <DropDown onClickLink={this.scrollToSection} />
-                </div>
-            );
-        };
+        return (
+            <DeskTopNav />
+        );
+
+        // if (this.state.screenSize > 992) {
+        //
+        // } else {
+        //     return (
+        //         <div>
+        //             <MobileNav onClickLink={this.scrollToSection} />
+        //             <DropDown onClickLink={this.scrollToSection} />
+        //         </div>
+        //     );
+        // };
     };
 };
 
